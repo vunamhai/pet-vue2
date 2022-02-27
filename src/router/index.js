69609c3da5ce34593c46d08b56cfbfd876974eb8
vunamhai <vunamhaimu@gmail.com>
2022-02-28 02:22:42 +0700

    add form login register forgot home page 28/2

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,9 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import Loginview from '../views/LoginView.vue'
+import SignUpView from '../views/SignUpView.vue'
+import ForgotView from '../views/ForgotView.vue'
+import NotFound from '../views/NotFoundView.vue'
 
 const routes = [
   {
@@ -8,17 +12,29 @@ const routes = [
     component: HomeView
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path: '/login',
+    name: 'login',
+    component: Loginview
+  },
+  {
+    path: '/signup',
+    name: 'signup',
+    component: SignUpView
+  },
+  {
+    path: '/forgot',
+    name: 'forgot',
+    component: ForgotView
+  },
+  {
+    path: '/:catchAll(.*)',
+    name: 'notFound',
+    component: NotFound
   }
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHashHistory(),
   routes
 })
 
